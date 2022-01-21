db9bd7400462421c399bf1e90fa03ef64138480c
Bogdan <bogdan.oloeriu@gmail.com>
2022-01-21 17:13:11 +0100

    moved interaction to interaction-logs

--- interaction/devnet.snippets.sh
+++ interaction/devnet.snippets.sh
@@ -5,12 +5,13 @@ ADDRESS=$(erdpy data load --key=address-devnet)
 DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-devnet)
 PROXY=https://devnet-gateway.elrond.com
 CHAINID=D
+MY_LOGS="interaction-logs"
 
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --arguments 0 --send --outfile="deploy-devnet.interaction.json" --proxy=${PROXY} --chain=${CHAINID} || return
+    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --arguments 0 --send --outfile="${MY_LOGS}/deploy-devnet.interaction.json" --proxy=${PROXY} --chain=${CHAINID} || return
 
-    TRANSACTION=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emitted_tx']['hash']")
-    ADDRESS=$(erdpy data parse --file="deploy-devnet.interaction.json" --expression="data['emitted_tx']['address']")
+    TRANSACTION=$(erdpy data parse --file="${MY_LOGS}/deploy-devnet.interaction.json" --expression="data['emitted_tx']['hash']")
+    ADDRESS=$(erdpy data parse --file="${MY_LOGS}/deploy-devnet.interaction.json" --expression="data['emitted_tx']['address']")
 
     erdpy data store --key=address-devnet --value=${ADDRESS}
     erdpy data store --key=deployTransaction-devnet --value=${TRANSACTION}
@@ -19,11 +20,8 @@ deploy() {
     echo "Smart contract address: ${ADDRESS}"
 }
 
-add() {
-    read -p "Enter number: " NUMBER
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --function="add" --arguments ${NUMBER}  --proxy=${PROXY} --chain=${CHAINID} --send
-}
-
-getSum() {
-    erdpy --verbose contract query ${ADDRESS} --function="getSum"  --proxy=${PROXY} --chain=${CHAINID} 
+issueToken() {
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --function="issueToken" \
+      --arguments 0 --proxy=${PROXY} --chain=${CHAINID} --send \
+      --outfile="${MY_LOGS}/issueToken.json"
 }
