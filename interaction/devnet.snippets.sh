57f6ab2345d4f53feb870a3cdb6320b50434722e
Bogdan <bogdan.oloeriu@gmail.com>
2022-01-21 18:13:57 +0100

    increased the gas limit

--- interaction/devnet.snippets.sh
+++ interaction/devnet.snippets.sh
@@ -7,8 +7,22 @@ PROXY=https://devnet-gateway.elrond.com
 CHAINID=D
 MY_LOGS="interaction-logs"
 
+#ISSUE_COST_HEX=$(echo -n 3 | xxd -p)
+TOKEN_NAME="TEST001"
+TOKEN_NAME_HEX=$(echo -n ${TOKEN_NAME} | xxd -p)
+TOKEN_REVERSE=$(xxd -r -p <<< "$TOKEN_NAME_HEX")
+ISSUE_TOKEN_ARGUMENTS="0 ${TOKEN_NAME_HEX} 5"
+
+
+haveFun(){
+    echo "${TOKEN_NAME}"
+    echo "${TOKEN_NAME_HEX}"
+    echo "${TOKEN_REVERSE}"
+    echo "${ISSUE_TOKEN_ARGUMENTS}"
+}
+
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --arguments 0 --send --outfile="${MY_LOGS}/deploy-devnet.interaction.json" --proxy=${PROXY} --chain=${CHAINID} || return
+    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=500000000 --arguments 0 --send --outfile="${MY_LOGS}/deploy-devnet.interaction.json" --proxy=${PROXY} --chain=${CHAINID} || return
 
     TRANSACTION=$(erdpy data parse --file="${MY_LOGS}/deploy-devnet.interaction.json" --expression="data['emitted_tx']['hash']")
     ADDRESS=$(erdpy data parse --file="${MY_LOGS}/deploy-devnet.interaction.json" --expression="data['emitted_tx']['address']")
@@ -22,6 +36,7 @@ deploy() {
 
 issueToken() {
     erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --function="issueToken" \
-      --arguments 0 --proxy=${PROXY} --chain=${CHAINID} --send \
+      --arguments ${ISSUE_TOKEN_ARGUMENTS}  \
+      --proxy=${PROXY} --chain=${CHAINID} --send \
       --outfile="${MY_LOGS}/issueToken.json"
 }
